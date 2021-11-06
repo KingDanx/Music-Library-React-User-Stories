f2f869264f868598ae64f86231f1b7b77a70b11d
Dan Prudhomme <dannyprud@yahoo.com>
2021-11-05 20:45:01 -0500

    edit search bar and search type to match theme

--- src/components/NavigationBar/NavigationBar.jsx
+++ src/components/NavigationBar/NavigationBar.jsx
@@ -12,13 +12,14 @@ const NavigationBar = () => {
               <span className="span-flex">dCC Music</span>
         <form className="form-inline my-2 my-lg-0 display-flex">
           <div className="dropdown">
-            <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              Dropdown button
-            </button>
-            <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-              <a className="dropdown-item" href="#">Action</a>
-              <a className="dropdown-item" href="#">Another action</a>
-              <a className="dropdown-item" href="#">Something else here</a>
+            <div>
+              <select className="search-select">
+                <option value="0">Search Type</option>
+                <option value="1">Title</option>
+                <option value="2">Artist</option>
+                <option value="3">Album</option>
+                <option value="4">Genre</option>
+              </select>
             </div>
           </div>
           <input className="form-control mr-sm-2 ib" type="search" placeholder="Search" aria-label="Search"/>
