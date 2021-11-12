bbd7d9a4437ea9b20c77fbf418ff8f28351da124
Dan Prudhomme <dannyprud@yahoo.com>
2021-11-11 22:07:51 -0600

    add form fields for data to be sent to backend.

--- src/components/AlbumCover/AlbumCover.jsx
+++ src/components/AlbumCover/AlbumCover.jsx
@@ -18,7 +18,7 @@ const AlbumCover = ({ currentSong, deleteASong }) => {
               type="button"
               className="btn btn-primary"
               data-toggle="modal"
-              data-target="#exampleModal"
+              data-target="#deleteModal"
             >
               <img
                 className="App-album-thumbdown btn-check:focus + .btn-primary, .btn-primary:focus "
@@ -29,7 +29,17 @@ const AlbumCover = ({ currentSong, deleteASong }) => {
           </div>
 
           <div className="App-little-circle"></div>
-          <div className="App-album-add-delete">+</div>
+          <div className="App-album-add-delete">
+            <button
+              type="button"
+              className="btn btn-primary App-album-add-delete"
+              style={{ display: "contents" }}
+              data-toggle="modal"
+              data-target="#exampleModalCenter"
+            >
+              +
+            </button>
+          </div>
         </div>
 
         <div>
@@ -40,7 +50,7 @@ const AlbumCover = ({ currentSong, deleteASong }) => {
       </div>
       <div
         className="modal fade"
-        id="exampleModal"
+        id="deleteModal"
         tabindex="-1"
         role="dialog"
         aria-labelledby="exampleModalLabel"
@@ -83,6 +93,93 @@ const AlbumCover = ({ currentSong, deleteASong }) => {
               >
                 Confirm Delete
               </button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div
+        className="modal fade"
+        id="exampleModalCenter"
+        tabindex="-1"
+        role="dialog"
+        aria-labelledby="exampleModalCenterTitle"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog modal-dialog-centered" role="document">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="exampleModalLongTitle">
+                Enter new song information:
+              </h5>
+              <button
+                type="button"
+                className="close x-button-popup"
+                data-dismiss="modal"
+                aria-label="Close"
+              >
+                <span aria-hidden="true">
+                  <b>X</b>
+                </span>
+              </button>
+            </div>
+            <div className="modal-body">
+              <form>
+                <div className="form-group">
+                  <label for="title">Title</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="title"
+                    aria-describedby="emailHelp"
+                    placeholder="Enter Song Title"
+                  />
+                </div>
+                <div className="form-group">
+                  <label for="artist">Artist</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="artist"
+                    placeholder="Enter the song's Artist"
+                  />
+                </div>
+                <div className="form-group">
+                  <label for="album">Album</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="album"
+                    aria-describedby="emailHelp"
+                    placeholder="Enter the Album"
+                  />
+                </div>
+                <div className="form-group">
+                  <label for="genre">Genre</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="genre"
+                    placeholder="Enter the genre"
+                  />
+                </div>
+                <div className="form-group">
+                  <label for="releaseDate">Release Date</label>
+                  <input
+                    type="date"
+                    className="form-control"
+                    id="releaseDate"
+                    placeholder="Enter the release date"
+                  />
+                </div>
+                <div className="form-check"></div>
+                <button
+                  type="submit"
+                  className="btn btn-primary App-popup-comfirm"
+                >
+                  Submit
+                </button>
+              </form>
             </div>
           </div>
         </div>
